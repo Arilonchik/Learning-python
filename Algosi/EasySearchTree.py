da1bb322299b5c3b7fe5efc4ace510e9909e32d7
Arilon <egorreal2011@gmail.com>
2020-03-26 14:56:45 +0300

    Done Algos 1.4.2

--- Algosi/EasySearchTree.py
+++ Algosi/EasySearchTree.py
@@ -1,21 +1,30 @@
+import sys
+
+sys.setrecursionlimit(10000)
+
+
 class BinaryTree:
     def __init__(self, n):
-        self.keys = [-1 for _ in range(n)]
-        self.left_children = [-1 for _ in range(n)]
-        self.right_children = [-1 for _ in range(n)]
+        self.keys = [-1 for _ in range(n+1)]
+        self.left_children = [-1 for _ in range(n+1)]
+        self.right_children = [-1 for _ in range(n+1)]
+        self.tree_option = None
 
     def add_v(self, key, ver, l_c, r_c):
         self.keys[ver] = key
         self.left_children[ver] = l_c
         self.right_children[ver] = r_c
 
-    def in_order(self, v, prev_v):
+    def in_order(self, v, prev_key=(-2**31)-1):
         if self.left_children[v] != -1:
-            self.in_order(self.left_children[v], v)
-        if prev_v > v:
-            return 'INCORRECT'
+            prev_key = self.in_order(self.left_children[v], prev_key)
+        if prev_key > self.keys[v]:
+            self.tree_option = 'INCORRECT'
+            return prev_key
+        prev_key = self.keys[v]
         if self.right_children[v] != -1:
-            self.in_order(self.right_children[v], v)
+            prev_key = self.in_order(self.right_children[v], self.keys[v])
+        return prev_key
 
 
 def main():
@@ -25,6 +34,7 @@ def main():
         key, l_c, r_c = map(int, input().split())
         b_t.add_v(key, i, l_c, r_c)
     b_t.in_order(0)
+    print(b_t.tree_option or 'CORRECT')
 
 
 if __name__ == '__main__':
